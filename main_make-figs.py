3b5c841a4e963fcc302cf9178072aabee52e8d94
lfierce2 <lfierce2@gmail.com>
2023-06-25 02:23:29 -0700

    Update main_make-figs.py

--- main_make-figs.py
+++ main_make-figs.py
@@ -3,17 +3,14 @@
 """
 Created on Mon Aug  8 16:53:54 2022
 
-@author: fier887
+@author: Laura Fierce
 """
 import process
 import numpy as np
 
-# output_dir = '/pic/projects/sooty2/fierce/output_LagrangeDroplets/'
 output_dir = '/Users/fier887/OneDrive - PNNL/Documents/students/FY22_Jesse-Andersen/output_LagrangeDroplets/'
 parcel_trace_dir = output_dir + 'parcel_traces/'
-# process.plot_spatial_trajectory(parcel_trace_dir=parcel_trace_dir)
 
-# parcel_nums = np.array([1,2,4])
 parcel_nums = np.array([18,1,4])
 process.plot_some_trajectories(parcel_nums,output_dir,avg_SS=0.,Slims=[-6,6],Dlims=[0.,15.],add_spatial_trajectories=False,plot_dir = 'figures/')
 avg_SS = np.linspace(-0.04,0.03,15)
