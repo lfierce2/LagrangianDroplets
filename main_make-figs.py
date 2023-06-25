1b8bb765945bc2ff41fe5b473152921c6b93bb38
lfierce2 <lfierce2@gmail.com>
2023-06-25 02:25:18 -0700

    Update main_make-figs.py

--- main_make-figs.py
+++ main_make-figs.py
@@ -8,10 +8,9 @@ Created on Mon Aug  8 16:53:54 2022
 import process
 import numpy as np
 
-output_dir = '/Users/fier887/OneDrive - PNNL/Documents/students/FY22_Jesse-Andersen/output_LagrangeDroplets/'
-parcel_trace_dir = output_dir + 'parcel_traces/'
+parcel_trace_dir = 'parcel_traces/'
 
 parcel_nums = np.array([18,1,4])
-process.plot_some_trajectories(parcel_nums,output_dir,avg_SS=0.,Slims=[-6,6],Dlims=[0.,15.],add_spatial_trajectories=False,plot_dir = 'figures/')
+process.plot_some_trajectories(parcel_nums,output_dir,avg_SS=0.,Slims=[-6,6],Dlims=[0.,15.],add_spatial_trajectories=False,plot_dir = 'figs/')
 avg_SS = np.linspace(-0.04,0.03,15)
-process.plot_activated_fraction_mean_SS(avg_SS, output_dir,plot_dir = output_dir+'figures/')
+process.plot_activated_fraction_mean_SS(avg_SS, output_dir,plot_dir = 'figs/')
